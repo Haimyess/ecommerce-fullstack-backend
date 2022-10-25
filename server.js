3418e287b9dc73bb6495ec869aedcdabdfe3aa71
Haim Yeshurun <haimyess@gmail.com>
2022-10-25 17:02:57 +0300

    added url before api in app.use products in server

--- server.js
+++ server.js
@@ -22,7 +22,10 @@ app.listen(process.env.PORT || 3001, () => {
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use("/api/products", products_router);
+app.use(
+  "https://ecommerce-fullstack-backend.herokuapp.com/api/products",
+  products_router
+);
 // app.use("/api/order", order_router);
 
 app.use(express.static(__dirname + "/public"));
